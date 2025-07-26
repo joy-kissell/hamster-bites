f6ed9d8b08b4a9bf4157176c80abca89832d72bd
Joy Kissell <dzp6qm@virginia.edu>
2025-07-26 16:31:35 -0400

    updating scraping for controversial foods section to add missing items

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -13,15 +13,29 @@ ids = ["fruits","vegetables", "proteins", "miscellaneous-foods", "controversial-
 for header in ids:
     section = soup.find(id = header)
     if section:
-        ul_element = section.find_next('ul')
-        #ul element is right before list starts
-        if ul_element:
-            items = [li.get_text(strip = True) for li in ul_element.find_all('li')]
-            for item in items:
-                food_data.append({
-                    'food': item,
+        if header == 'controversial-foods':
+            next_section = soup.find(id = 'dangerous-foods')
+            current_element = section
+            #only for controversial-foods header
+            while current_element and current_element != next_section:
+                food_names = [tag.get_text(strip=True) for tag in (current_element.find_all('strong') if hasattr(current_element, 'find_all') else [])]
+                for food in food_names:
+                    food_data.append({
+                    'food': food,
                     'type': header.replace('-', ' ').title(),
-                    'safe': 'no' if header == 'dangerous-foods' else 'controversial' if header == 'controversial-foods' else 'yes'
+                    'safe': 'controversial'
                 })
+                current_element = current_element.next_sibling
+        else:         
+            ul_element = section.find_next('ul')
+            #ul element is right before list starts
+            if ul_element:
+                items = [li.get_text(strip = True) for li in ul_element.find_all('li')]
+                for item in items:
+                    food_data.append({
+                        'food': item,
+                        'type': header.replace('-', ' ').title(),
+                        'safe': 'no' if header == 'dangerous-foods' else 'yes'
+                    })
 df = pd.DataFrame(data=food_data)
-df.to_csv('hamster_foods.csv', index=False)
+df.to_csv('data/hamster_foods.csv', index=False)
