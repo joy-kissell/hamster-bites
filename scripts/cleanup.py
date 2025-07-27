f4c1a7a0ea96a5d5f255c00f22a757b1737fb7f2
Joy Kissell <dzp6qm@virginia.edu>
2025-07-26 23:53:45 -0400

    controversial column added and controversial type deleted, readding foods back in so no duplicates

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -8,6 +8,21 @@ duplicates = cleanData[cleanData.duplicated(subset=['food'], keep=False)]
 #only print food name
 print("duplicates are:" + duplicates['food'])
 
+cleanData = cleanData[cleanData['food'] != 'perfectly safe']
 controversial_foods = cleanData[cleanData['type'] == 'Controversial Foods']['food'].tolist()
 print(f"controversial foods: {controversial_foods}")
-cleanData = cleanData[cleanData['food'] != 'perfectly safe']
+
+
+#getting rid of controversial food type
+foodData = cleanData[cleanData['type']!= 'Controversial Foods'].copy()
+#boolean column for if food is controversial or not
+foodData['controversial'] = foodData['food'].isin(controversial_foods)
+for food in controversial_foods:
+    #if food is not in cleaned dataframe but is in controversial foods
+    if food not in foodData['food'].values:
+        #adding row to end of dataframe
+        next_index = foodData.index.max() + 1
+        foodData.loc[next_index]=[food, "Miscellaneous Foods", 'yes', True]
+        last_row = foodData.iloc[-1]
+
+print(foodData[foodData['controversial']]['food'])
