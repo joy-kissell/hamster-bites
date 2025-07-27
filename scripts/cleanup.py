4f5a2503a08f3fbb077780297bc80c74e0e3c60f
Joy Kissell <dzp6qm@virginia.edu>
2025-07-26 23:11:42 -0400

    removing nonfood values and duplicates

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -3,3 +3,11 @@ cleanData = pd.read_csv('data/hamster_foods.csv')
 print(cleanData.head())
 print(cleanData.describe())
 print(cleanData.dtypes)
+
+duplicates = cleanData[cleanData.duplicated(subset=['food'], keep=False)]
+#only print food name
+print("duplicates are:" + duplicates['food'])
+
+controversial_foods = cleanData[cleanData['type'] == 'Controversial Foods']['food'].tolist()
+print(f"controversial foods: {controversial_foods}")
+cleanData = cleanData[cleanData['food'] != 'perfectly safe']
